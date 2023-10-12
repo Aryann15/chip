c72f3e9178bddef0c54feafb4eb7ca5a70dc113d
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-13 03:40:22 +0530

    loading the3 youtube video

--- server/main.py
+++ server/main.py
@@ -1,18 +1,25 @@
 from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-
+from langchain.document_loaders import YoutubeLoader
+from langchain.llms import OpenAI
+from langchain.chains.summarize import load_summarize_chain
 
 app= Flask (__name__)
 CORS(app)
 
-@app.route("/review",method = ["POST"])
+@app.route("/review",methods= ["POST"])
 def prod_review():
+    load_dotenv()
     product = request.form["product"]
 
     if product:
-        prompt = (f"youtube link: {product}")
-        return prompt
+        link = (f"youtube link: {product}")
+        loader = YoutubeLoader.from_youtube_url(link,add_video_info = True)
+    
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)

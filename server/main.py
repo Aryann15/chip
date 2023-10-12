87ba52d95045e845187c66eef683c9a645d8b495
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-13 04:51:19 +0530

    to take multiple links in alist

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ from langchain.document_loaders import YoutubeLoader
 from langchain.llms import OpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+import json
 
 app= Flask (__name__)
 CORS(app)
@@ -13,18 +13,27 @@ CORS(app)
 @app.route("/review",methods= ["POST"])
 def prod_review():
     load_dotenv()
-    product = request.form["product"]
+    data = json.loads(request.data.decode("utf-8"))
+    urls = data["product"]
 
-    if product:
-        link = (f"youtube link: {product}")
-        loader = YoutubeLoader.from_youtube_url(product,add_video_info = True)
-        result = loader.load();
-        llm = OpenAI(temperature=0.5)
+    if urls:
+        # link = (f"youtube link: {product}")
         text_splitter = RecursiveCharacterTextSplitter (chunk_size = 1300 , chunk_overlap = 200)
-        texts = text_splitter.split_documents(result)
-        chain = load_summarize_chain (llm , chain_type="map_reduce",verbose=True)
-        summary = chain.run(texts)
-        return summary
+        url_lists = [urls]
+        llm = OpenAI(temperature=0.5)
+        texts = []
+       
+
+        return url_lists    
+        # for url in url_lists:
+        #     loader = YoutubeLoader.from_youtube_url(url,add_video_info = True)
+        #     result = loader.load();
+        #     texts.extend(text_splitter.split_documents(result))
+        
+        # # texts = text_splitter.split_documents(result)
+        # chain = load_summarize_chain (llm , chain_type="map_reduce",verbose=True)
+        # summary = chain.run(texts)
+        # return summary
 
 
 if __name__ == "__main__":

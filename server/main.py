a8aebe183e40eaac4d06f44b48930433a3b27f4b
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-13 05:03:04 +0530

    prompt for the texts

--- server/main.py
+++ server/main.py
@@ -5,35 +5,46 @@ from langchain.document_loaders import YoutubeLoader
 from langchain.llms import OpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import json
 
-app= Flask (__name__)
+
+app = Flask(__name__)
 CORS(app)
 
-@app.route("/review",methods= ["POST"])
+
+@app.route("/review", methods=["POST"])
 def prod_review():
     load_dotenv()
-    data = json.loads(request.data.decode("utf-8"))
-    urls = data["product"]
+
+    urls = request.form["product"]
 
     if urls:
         # link = (f"youtube link: {product}")
-        text_splitter = RecursiveCharacterTextSplitter (chunk_size = 1300 , chunk_overlap = 200)
-        url_lists = [urls]
-        llm = OpenAI(temperature=0.5)
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=["\n\n", "\n"], chunk_size=1500, chunk_overlap=200
+        )
+        url_lists = ["", ""]
+        llm = OpenAI(temperature=0)
         texts = []
-       
 
-        return url_lists    
-        # for url in url_lists:
-        #     loader = YoutubeLoader.from_youtube_url(url,add_video_info = True)
-        #     result = loader.load();
-        #     texts.extend(text_splitter.split_documents(result))
-        
-        # # texts = text_splitter.split_documents(result)
-        # chain = load_summarize_chain (llm , chain_type="map_reduce",verbose=True)
-        # summary = chain.run(texts)
-        # return summary
+        for url in url_lists:
+            loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+            result = loader.load()
+            texts.extend(text_splitter.split_documents(result))
+
+        # texts = text_splitter.split_documents(result)
+        map_prompt = """
+
+        Write a detailed review of the product using the followinf text:
+
+        "{texts}"
+
+        SUMMARY:
+
+        """
+
+        chain = load_summarize_chain(llm, chain_type="map_reduce", verbose=True)
+        summary = chain.run(texts)
+        return summary
 
 
 if __name__ == "__main__":

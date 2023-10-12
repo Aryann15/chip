a63dffc80cb75015213ff9a5c9c2075e6712dc73
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-13 05:23:51 +0530

    basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes , Link} from "react-router-dom"
+import Review from './pages/Review';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/review">Review</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Routes>
+      <Route path ="/" element={<Home/>}/>
+      <Route path ="/review" element={<Review />}/>
+
+    </Routes>
+
+  </div>
 }
 
 export default App;

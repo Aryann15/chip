85d0f0053dc854192d517f4957856acad4951d39
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-13 04:11:16 +0530

    using recursive char text splitter to split the long video transcript in mulltiple shprt transcripts and determine the summary

--- server/main.py
+++ server/main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from langchain.document_loaders import YoutubeLoader
 from langchain.llms import OpenAI
 from langchain.chains.summarize import load_summarize_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 
 app= Flask (__name__)
 CORS(app)
@@ -15,10 +17,14 @@ def prod_review():
 
     if product:
         link = (f"youtube link: {product}")
-        loader = YoutubeLoader.from_youtube_url(link,add_video_info = True)
-    
-
-
+        loader = YoutubeLoader.from_youtube_url(product,add_video_info = True)
+        result = loader.load();
+        llm = OpenAI(temperature=0.5)
+        text_splitter = RecursiveCharacterTextSplitter (chunk_size = 1300 , chunk_overlap = 200)
+        texts = text_splitter.split_documents(result)
+        chain = load_summarize_chain (llm , chain_type="map_reduce",verbose=True)
+        summary = chain.run(texts)
+        return summary
 
 
 if __name__ == "__main__":

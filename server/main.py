6a82df9eab70a5654428f3b22bc265edb698a625
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-13 06:22:16 +0530

    Flask backend init

---
+++ server/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+
+app= Flask (__name__)
+CORS(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

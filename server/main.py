cc6d4fb3ed005abbe0b5dd47e6eadf5edc2b9fbf
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-13 06:27:01 +0530

    take the input product name

--- server/main.py
+++ server/main.py
@@ -6,6 +6,13 @@ from dotenv import load_dotenv
 app= Flask (__name__)
 CORS(app)
 
+@app.route("/review",method = ["POST"])
+def prod_review():
+    product = request.form["product"]
+
+    if product:
+        prompt = (f"youtube link: {product}")
+        return prompt
 
 if __name__ == "__main__":
     app.run(debug=True)

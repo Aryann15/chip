da2110f05e89644025222011c11810eb32a4e532
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-19 23:58:51 +0530

    route to fetch urls from product

--- server/main.py
+++ server/main.py
@@ -16,16 +16,12 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route("/review", methods=["POST"])
-def prod_review():
+@app.route("/", methods =["POST"])
+def product():
     load_dotenv()
-
     api_key = os.environ.get("YOUTUBE_API_KEY")
-    openai_api_key = os.environ.get("OPENAI_API_KEY")
     product = request.form["product"]
-    query = request.form["query"]
-    if query and product:
+    if product:
         youtube = build ('youtube', 'v3', developerKey = api_key)
         product = product
         channel_ids = [
@@ -43,48 +39,76 @@ def prod_review():
             youtube_link = "https://www.youtube.com/watch?v=" + link
             url_lists.append(youtube_link)
 
-        llm = ChatOpenAI(temperature=0)
-        texts = ""
+        return url_lists
 
-        for url in url_lists:
-            loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
-            result = loader.load()
-            for document in result:
-                texts += document.page_content + " "
+# @app.route("/review", methods=["POST"])
+# def prod_review():
+#     load_dotenv()
 
-        text_splitter = CharacterTextSplitter(
-            separator=" ", chunk_size=1500, chunk_overlap=200
-        )
-        chunks = text_splitter.split_text(texts)
-        with get_openai_callback() as cb:
-            embeddings = OpenAIEmbeddings()
-            print(cb)
-        vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
-        print("vs init")
+#     api_key = os.environ.get("YOUTUBE_API_KEY")
+#     openai_api_key = os.environ.get("OPENAI_API_KEY")
+#     product = request.form["product"]
+#     query = request.form["query"]
+#     if query and product:
+#         youtube = build ('youtube', 'v3', developerKey = api_key)
+#         product = product
+#         channel_ids = [
+#             "UCBJycsmduvYEL83R_U4JriQ", 
+#             "UCMiJRAwDNSNzuYeN2uWa0pA",
+#             "UCXuqSBlHAE6Xw-yeJA0Tunw"
+#             ]
+#         url_lists= []
 
-        memory = ConversationBufferMemory(
-            memory_key="chat_history", k=3, return_messages=True
-        )
-        print("finished conversational memory")
-        conv_chain = ConversationalRetrievalChain.from_llm(
-            llm=llm,
-            retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
-            memory=memory,
-        )
-        print("finished conversational chain")
 
-        answer = conv_chain({"question": query})
-        if "chat_history" in answer and len(answer["chat_history"]) > 0:
-            response = {"answer": answer["chat_history"][-1].content, "question": query}
-        else:
-            response = {"answer": "No answer found", "question": query}
+#         for channel_id in channel_ids:
+#             req = youtube.search().list (q= product + " review" , part= "snippet" , channelId=channel_id , type = 'video', maxResults = 1)
+#             res = req.execute()
+#             link = res.get("items")[0]["id"]["videoId"]
+#             youtube_link = "https://www.youtube.com/watch?v=" + link
+#             url_lists.append(youtube_link)
 
-        return jsonify(response)
+#         llm = ChatOpenAI(temperature=0)
+#         texts = ""
 
-        # print (answer)
-        # return (answer)
+#         for url in url_lists:
+#             loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+#             result = loader.load()
+#             for document in result:
+#                 texts += document.page_content + " "
+
+#         text_splitter = CharacterTextSplitter(
+#             separator=" ", chunk_size=1500, chunk_overlap=200
+#         )
+#         chunks = text_splitter.split_text(texts)
+#         with get_openai_callback() as cb:
+#             embeddings = OpenAIEmbeddings()
+#             print(cb)
+#         vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+#         print("vs init")
+
+#         memory = ConversationBufferMemory(
+#             memory_key="chat_history", k=3, return_messages=True
+#         )
+#         print("finished conversational memory")
+#         conv_chain = ConversationalRetrievalChain.from_llm(
+#             llm=llm,
+#             retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
+#             memory=memory,
+#         )
+#         print("finished conversational chain")
+
+#         answer = conv_chain({"question": query})
+#         if "chat_history" in answer and len(answer["chat_history"]) > 0:
+#             response = {"answer": answer["chat_history"][-1].content, "question": query}
+#         else:
+#             response = {"answer": "No answer found", "question": query}
+
+#         return jsonify(response)
+
+#         # print (answer)
+#         # return (answer)
         
-    # return url_lists
+#     # return url_lists
 
 
 if __name__ == "__main__":

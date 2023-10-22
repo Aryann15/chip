3376d1f4fd80b1ccfd169bb5b0ca8e258943b2b0
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-22 23:57:30 +0530

    will take all 1,2 ...5 star ratings individually in tat way it  works

--- server/amazon.py
+++ server/amazon.py
@@ -29,15 +29,25 @@ def get_asin(url):
 asin= get_asin(url)
 
 
-def reviews():
+def reviews(asin):
+    reviewList = []
     headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
-    review_url= 'https://www.amazon.in/product-reviews/B08N5W4NNB/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=positive'
+    review_url= f'https://www.amazon.in/product-reviews/{asin}/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=four_star'
     res = requests.get(review_url, headers=headers)
     soup = BeautifulSoup(res.text, "html.parser")
     reviews = soup.find_all("div", {"data-hook": "review"})
+    
+    for item in reviews:
+        review = {
+            'title': item.find('a', {'data-hook': 'review-title'}).text.strip('\n')[19:],
+            'rating': float(item.find('i', {'data-hook': 'review-star-rating'}).text.replace('out of 5 stars', '').strip()),
+            'body': item.find('span', {'data-hook': 'review-body'}).text.strip(),
+        }
+        reviewList.append(review)
+    return len(reviewList)
 
-    return reviews
+    
  
 
-html = reviews()
-print(html)
+
+print(reviews('B08N5W4NNB'))

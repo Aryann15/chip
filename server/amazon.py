0c3b759e0d6b2d678cf883b11735c9c26361497e
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-23 00:02:19 +0530

    10 reviews for 1,2,...5 star reviews

--- server/amazon.py
+++ server/amazon.py
@@ -2,7 +2,7 @@ from requests_html import HTMLSession
 import requests
 from bs4 import BeautifulSoup
 
-product= "Macbook air m1"
+product = "Macbook air m1"
 url_product = ""
 
 for letter in product:
@@ -12,42 +12,57 @@ for letter in product:
     else:
         url_product = url_product + "+"
 
-url = "https://www.amazon.in/s?k="+ url_product
+url = "https://www.amazon.in/s?k=" + url_product
 # print(url)
 s = HTMLSession()
 
+
 def get_asin(url):
-    r=s.get(url)
-    products= r.html.find('div[data-asin]')
+    r = s.get(url)
+    products = r.html.find("div[data-asin]")
     asins = []
     for product in products:
-        if product.attrs['data-asin'] != '':
-            asins.append(product.attrs['data-asin'])
-    actual_url= "https://www.amazon.in/dp/"+asins[0]
+        if product.attrs["data-asin"] != "":
+            asins.append(product.attrs["data-asin"])
+    actual_url = "https://www.amazon.in/dp/" + asins[0]
     return actual_url
 
-asin= get_asin(url)
+
+asin = get_asin(url)
 
 
 def reviews(asin):
     reviewList = []
-    headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
-    review_url= f'https://www.amazon.in/product-reviews/{asin}/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=four_star'
-    res = requests.get(review_url, headers=headers)
-    soup = BeautifulSoup(res.text, "html.parser")
-    reviews = soup.find_all("div", {"data-hook": "review"})
-    
-    for item in reviews:
-        review = {
-            'title': item.find('a', {'data-hook': 'review-title'}).text.strip('\n')[19:],
-            'rating': float(item.find('i', {'data-hook': 'review-star-rating'}).text.replace('out of 5 stars', '').strip()),
-            'body': item.find('span', {'data-hook': 'review-body'}).text.strip(),
-        }
-        reviewList.append(review)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+    }
+    review_url = [
+        
+        f"https://www.amazon.in/product-reviews/{asin}/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=five_star",
+        f"https://www.amazon.in/product-reviews/{asin}/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=four_star",
+        f"https://www.amazon.in/product-reviews/{asin}/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=three_star",
+        f"https://www.amazon.in/product-reviews/{asin}/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=two_star",
+        f"https://www.amazon.in/product-reviews/{asin}/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber=1&filterByStar=one_star",
+    ]
+    for url in review_url:
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.text, "html.parser")
+        reviews = soup.find_all("div", {"data-hook": "review"})
+
+        for item in reviews:
+            review = {
+                "title": item.find("a", {"data-hook": "review-title"}).text.strip("\n")[
+                    19:
+                ],
+                "rating": float(
+                    item.find("i", {"data-hook": "review-star-rating"})
+                    .text.replace("out of 5 stars", "")
+                    .strip()
+                ),
+                "body": item.find("span", {"data-hook": "review-body"}).text.strip(),
+            }
+            reviewList.append(review)
     return len(reviewList)
 
-    
- 
 
-
-print(reviews('B08N5W4NNB'))
+print(reviews("B08N5W4NNB"))
